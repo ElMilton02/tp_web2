608b43c6e269ec7228c864b2df65d6fee0d86a23
Milton Palucci <miltonpalucci02@gmail.com>
2024-10-12 18:47:40 -0300

    se añadieron todos los models

---
+++ app/models/DestinoModel.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once './config.php';
+require_once './apps/models/Model.php';
+
+class DestinoModel extends Model
+{
+
+    function getDestinos()
+    {
+
+        $query = $this->db->prepare('SELECT * FROM destinos');
+        $query->execute();
+
+      
+        $destinos = $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $destinos;
+    }
+
+    function deleteDestino($idDestino)
+    {
+        $query = $this->db->prepare('DELETE FROM destinos WHERE id_destino = ?');
+        $query->execute([$idDestino]);
+    }
+
+    function insertDestino($destino)
+    {
+        $query = $this->db->prepare('INSERT INTO destinos (destino) VALUES(?)');
+        $query->execute([$destino]);
+        return $this->db->lastInsertId();
+    }
+
+    public function getDestinoById($id)
+    {
+        $query = $this->db->prepare('SELECT * FROM destinos WHERE id_destino = ?');
+        $query->execute([$id]);
+
+        // Obtener la categoría como un objeto
+        $destino = $query->fetch(PDO::FETCH_OBJ);
+
+        return $destino;
+    }
+
+    public function modifyDestino($id, $destino, $img)
+    {
+        $query = $this->db->prepare('UPDATE Destinos SET destino = ?, img = ? WHERE id_destino = ?');
+        $query->execute([$id, $destino, $img]);
+    }
+}
---
+++ app/models/ViajeModel.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once './config.php';
+require_once './apps/models/Model.php';
+
+class viajeModel extends Model
+{
+
+    function getViajesByDestino($href)
+    {
+        $query = $this->db->prepare('SELECT * FROM viajes WHERE id_destino = ?');
+        $query->execute([$href]);
+
+        $viajes = $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $viajes;
+    }
+
+    function deleteViaje($idViaje)
+    {
+        $query = $this->db->prepare('DELETE FROM viajes WHERE id_viaje = ?');
+        $query->execute([$idViaje]);
+    }
+
+    function insertViaje($fecha, $id_destino, $hora)
+    {  
+        $query = $this->db->prepare('INSERT INTO viajes (fecha, destino, hora) VALUES(?, ?, ?)');
+        $query->execute([$fecha, $id_destino, $hora]);
+    
+        return $this->db->lastInsertId();
+    }
+
+    public function modifyViaje($newFecha, $newDestino, $newHora)
+    {  
+        $query = $this->db->prepare('UPDATE viajes SET fecha = ?, id_destino = ?, hora = ? WHERE id = ?');
+        $query->execute([$newFecha, $newDestino, $newHora]);
+    }
+}
