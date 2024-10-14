d03f4795db1dcb5f69caff5bfcb200c00a0b13b3
Milton Palucci <miltonpalucci02@gmail.com>
2024-10-14 11:01:19 -0300

    se armo el router

--- router.php
+++ router.php
@@ -1,7 +1,9 @@
 <?php
 
-require_once 'config.php';
-require_once './app/controllers/auth.controller.php';
+require_once 'apps/controllers/HomeController.php';
+require_once 'apps/controllers/DestinoController.php';
+require_once 'apps/controllers/ViajeController.php';
+require_once 'apps/controllers/AuthController.php';
 
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
@@ -17,11 +19,73 @@ if (!empty( $_GET['action'])) {
 $params = explode('/', $action);
 
 #Dependiendo de la acción solicitada (primer valor de $params), el código invoca el controlador correspondiente y ejecuta la acción.
-#Por ejemplo, si la acción es 'listar', crea una instancia de VinotecaController y llama a su método showVinos()
 switch ($params[0]) { 
-    case 'home':
-        $controller= new HomeController();
+    case 'home': //Muestra el Home
+        $controller = new HomeController();
         $controller->showHome();
         break;
-    
+    case 'destinos': 
+        $controller = new DestinoController();
+        $controller->showDestinos();
+        break;
+    case 'viajeByDestino': 
+        $controller = new ViajeController();
+        $controller->showViajesByDestinoId($params[1]);
+        break;
+    case 'eliminarViaje':
+        $controller = new ViajeController();
+        $controller->removeViajes($params[1], $params[2]);
+        break;
+    case 'agregarViaje':
+        $controller = new ViajeController();
+        $controller->addViaje($params[1]);
+        break;
+    case 'actualizarViaje ':
+        $controller = new ViajeController();
+        $controller->updateViajes($params[1]);
+        break;
+    case 'editarViaje':
+        $controller = new ViajeController();
+        $controller->editViajes($params[1]);
+        break;
+    case 'eliminarDestino':
+        $controller = new DestinoController();
+        $controller->removeDestino($params[1]);
+        break;
+    case 'agregarDestino':
+        $controller = new DestinoController();
+        $controller->addDestino();
+        break;
+    case 'actualizarDestino':
+        $controller = new DestinoController();
+        $controller->updateDestino($params[1]);
+        break;
+    case 'editDestino':
+        $controller = new DestinoController();
+        $controller->editDestino($params[1]);
+        break;
+    case 'login':
+        $controller = new AuthController();
+        $controller->showLogin();
+        break;
+    case 'singup': 
+        $controller = new AuthController();
+        $controller->showSingup();
+        break;
+    case 'registro': 
+        $controller = new AuthController();
+        $controller->upUser();
+        break;
+    case 'auth': 
+        $controller = new AuthController();
+        $controller->auth();
+        break;
+    case 'logOut': 
+        $controller = new AuthController();
+        $controller->logOut();
+        break;
+    default:
+        $controller = new ErrorController();
+        $controller->showError404($error);
+        break;
 }
