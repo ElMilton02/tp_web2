207a4af28ade32dc38282c1049e86f2d4652c0b2
Milton Palucci <miltonpalucci02@gmail.com>
2024-10-14 10:33:46 -0300

    se modifico los controllers y se añadio el error controller

---
+++ app/controllers/ErrorController.php
@@ -0,0 +1,59 @@
+<?php
+
+class ErrorController
+{
+
+  public function showErrorInvalidUser($error)
+  {
+
+    require_once './templates/login.phtml';
+  }
+
+  public function showErrorNonData($error)
+  {
+
+    require_once './templates/login.phtml';
+  }
+
+  public function showErrorNonDataDestino($error, $model)
+  {
+    $categories = $model->getDestinos();
+    $view = new DestinoView();
+    $view->showDestinos($categories, $error);
+  }
+
+  public function showErrorInsert($error)
+  {
+
+    require_once './templates/login.phtml';
+  }
+
+  public function showErrorDelete($error, $model)
+  {
+    $destinos = $model->getDesinos();
+    $view = new DestinoView();
+    $view->showDestinos($destinos, $error);
+  }
+  
+  public function showErrorNonUser($error, $page)
+  {
+
+    if ($page == 'home') {
+      $view = new HomeView();
+      $view->showHome($error);
+    }
+  }
+
+  public function showError404($error)
+  {
+    $view = new HomeView();
+    $view->showHome($error);
+  }
+
+  public function showErrorNonDataViajes($error, $model, $destinoId)
+  {
+    $listViajes = $model->getViajesByDestino($destinoId);
+    $view = new ViajeView();
+    $view->showViajesByDestinoId($listViajes, $destinoId, $error = null);
+  }
+}
--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,5 +5,16 @@ require_once './apps/helpers/AuthHelper.php';
 
 class HomeController
 {
+    private $view;
 
+    public function __construct()
+    {
+        AuthHelper::verify();
+        $this->view = new HomeView();
+    }
+
+    public function showHome()
+    {
+        $this->view->showHome();
+    }
 }
